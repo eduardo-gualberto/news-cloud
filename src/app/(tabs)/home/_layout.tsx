d898e75bace1c39067654a14d416a490a95b7849
Eduardo Gualberto <58743989+eduardo-gualberto@users.noreply.github.com>
2024-01-18 19:26:16 -0300

    feat: explores expo-router file based routing system

--- src/app/(tabs)/home/_layout.tsx
+++ src/app/(tabs)/home/_layout.tsx
@@ -2,6 +2,8 @@ import { Stack } from "expo-router";
 
 export default function HomeLayout() {
     return (
-        <Stack/>
+        <Stack screenOptions={{ statusBarTranslucent: false, statusBarColor: '#000' }}>
+            <Stack.Screen name="news-details" options={{ presentation: 'modal', headerShown: false }} />
+        </Stack>
     )
 }
