26942198d192a6bd0a5c10ff3fbae6cb73a154ac
Eduardo Gualberto <58743989+eduardo-gualberto@users.noreply.github.com>
2024-01-22 19:14:50 -0300

    feat: adds in clicking possibility to categories and setsup preact signals

--- src/app/(tabs)/home/index.tsx
+++ src/app/(tabs)/home/index.tsx
@@ -1,9 +1,10 @@
 import { Link, Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import appStyles from '../../styles';
 
 import HorizontalCarousel from '../../components/HorizontalCarousel';
+import { signal } from '@preact/signals-react';
 
 const { smallText, mediumText, whiteText } = appStyles
 
@@ -11,20 +12,26 @@ const generateRandomHexColor = () => {
   return `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, '0')}`
 }
 
+const fuck = signal(0)
+
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ headerShown: false }} />
       <View style={styles.container}>
         <HorizontalCarousel items={[1, 2, 3, 4, 5]} itemGap={15} itemRenderer={(item, key) => (
-          <View style={{ borderWidth: 2, borderColor: generateRandomHexColor(), height: 35, width: 150, borderRadius: 10, justifyContent: 'center', alignItems: 'center' }} key={key} >
+          <TouchableOpacity
+            key={key}
+            onPress={() => {
+              console.log(item, key);
+              fuck.value = key
+            }}
+            style={{ borderWidth: 2, borderColor: generateRandomHexColor(), height: 35, width: 150, borderRadius: 10, justifyContent: 'center', alignItems: 'center' }}
+          >
             <Text style={[smallText, whiteText]}> card {item}</Text>
-          </View>
+          </TouchableOpacity>
         )} />
-        {/* <Text style={[mediumText, whiteText]}>Home Page</Text>
-        <Link href={"/home/news-details"}>
-          <Text style={[smallText, whiteText]}>Go to news detail page</Text>
-        </Link> */}
+        <Text style={[smallText, whiteText]}>Selecionado: {fuck.value}</Text>
         <StatusBar style="auto" />
       </View>
     </>
