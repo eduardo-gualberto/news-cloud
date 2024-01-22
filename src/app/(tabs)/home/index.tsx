9a2ff4f76045ee8794df30872a3aa9b295f61078
Eduardo Gualberto <58743989+eduardo-gualberto@users.noreply.github.com>
2024-01-22 18:33:16 -0300

    feat: adds youtube like category filter to home page

--- src/app/(tabs)/home/index.tsx
+++ src/app/(tabs)/home/index.tsx
@@ -3,17 +3,28 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import appStyles from '../../styles';
 
+import HorizontalCarousel from '../../components/HorizontalCarousel';
+
 const { smallText, mediumText, whiteText } = appStyles
+
+const generateRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 0x1000000).toString(16).padStart(6, '0')}`
+}
 
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ headerShown: false }} />
       <View style={styles.container}>
-        <Text style={[mediumText, whiteText]}>Home Page</Text>
+        <HorizontalCarousel items={[1, 2, 3, 4, 5]} itemGap={15} itemRenderer={(item, key) => (
+          <View style={{ borderWidth: 2, borderColor: generateRandomHexColor(), height: 35, width: 150, borderRadius: 10, justifyContent: 'center', alignItems: 'center' }} key={key} >
+            <Text style={[smallText, whiteText]}> card {item}</Text>
+          </View>
+        )} />
+        {/* <Text style={[mediumText, whiteText]}>Home Page</Text>
         <Link href={"/home/news-details"}>
           <Text style={[smallText, whiteText]}>Go to news detail page</Text>
-        </Link>
+        </Link> */}
         <StatusBar style="auto" />
       </View>
     </>
@@ -26,6 +37,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#222',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 50
   },
 });
---
+++ src/app/components/HorizontalCarousel/index.tsx
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet, ScrollView } from 'react-native'
+import React from 'react'
+
+type HorizontalCarouselProps<T> = {
+    items: T[],
+    itemRenderer: (item: T, key: number) => React.JSX.Element,
+    itemGap?: number
+}
+
+export default function HorizontalCarousel<T>(props: HorizontalCarouselProps<T>) {
+    return (
+        <View style={styles.containter}>
+            <ScrollView horizontal showsHorizontalScrollIndicator={false} centerContent contentContainerStyle={{ gap: props.itemGap ?? 5 }}>
+                {props.items.map((item, key) => props.itemRenderer(item, key))}
+            </ScrollView>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    containter: {
+        width: "100%",
+        padding: 5,
+        paddingLeft: 20
+    }
+})
